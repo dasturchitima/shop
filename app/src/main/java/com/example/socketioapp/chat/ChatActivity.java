b8e9647326f6a6e51e552c70db908ff32982c7a5
Dasturchitima <fcbnews_uz@bk.ru>
2019-09-17 10:05:28 +0500

    Update ChatActivity.java

--- app/src/main/java/com/example/socketioapp/chat/ChatActivity.java
+++ app/src/main/java/com/example/socketioapp/chat/ChatActivity.java
@@ -70,7 +70,7 @@ public class ChatActivity extends AppCompatActivity implements ChatContract.View
                 Injection.provideSchedulerProvider(),
                 Injection.providesRepository(this));
     }
-//ss
+//Nima gapla
     @Override
     protected void onPause() {
         super.onPause();
